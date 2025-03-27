72fe136e0ea69ce3c5995a70685cef08d547446a
manthansubhash01 <manthanziman20@gmail.com>
2025-03-27 18:35:46 +0530

    updated forms

--- Frontend/ideavault/src/components/Registration.jsx
+++ Frontend/ideavault/src/components/Registration.jsx
@@ -21,8 +21,8 @@ function Registration() {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
-              body: JSON.stringify(formData),
             },
+            body: JSON.stringify(formData)
           }
         );
 
