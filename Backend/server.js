6653ecc1fdc889f00d6b571161ed4a13b63fa9e4
manthansubhash01 <manthanziman20@gmail.com>
2025-03-27 10:26:25 +0530

    updated cors in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDb();
+
+app.use(
+  cors({
+    origin: "*", // Allow all origins (Change this to your frontend domain for security)
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
 app.use(express.json());
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"))
 
-app.use(cors())
-app.use(express.json())
+app.get("/",(req,res) =>{
+  res.send("Server is running")
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
