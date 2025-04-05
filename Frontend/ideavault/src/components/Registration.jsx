bb8aade09c44bbbcedaf1af0638f87f6eb22e075
manthansubhash01 <manthanziman20@gmail.com>
2025-04-05 18:32:21 +0530

    set the description length

--- Frontend/ideavault/src/components/Registration.jsx
+++ Frontend/ideavault/src/components/Registration.jsx
@@ -184,6 +184,7 @@ function Registration() {
                 value={formData.projectDescription}
                 onChange={handleChange}
                 className="w-full px-4 py-2 text-white border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                minLength={100}
                 required
               ></input>
             </div>
